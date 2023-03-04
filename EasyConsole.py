461a168922167b48b69666d22646bd6ecf3e420d
964293341 <asdawej123@163.com>
2023-03-04 21:53:55 +0800

    ADD const & let & del & run & cmd
    - ADD do API func
    
    - REVISE README

--- EasyConsole.py
+++ EasyConsole.py
@@ -4,6 +4,7 @@
 from __future__ import annotations
 
 import cmd
+import os
 import pickle as pkl
 import pathlib as plb
 
@@ -35,8 +36,15 @@ class EasyConsole(cmd.Cmd):
     path_ptr = plb.Path('.').absolute()
     son_paths: list[plb.Path] = [x for x in path_ptr.iterdir()]
     path_num = len(son_paths)
-    fp_const=open(r'ConstPaths.dat', 'r+b')
+    try:
+        fp_const = open(r'ConstPaths.dat', 'rb')
+    except:
+        fp_const = open(r'ConstPaths.dat', 'wb')
+        pkl.dump({}, fp_const)
+        fp_const.close()
+        fp_const = open(r'ConstPaths.dat', 'rb')
     const_def_tab: dict[plb.Path] = pkl.load(fp_const)
+    fp_const.close()
     path_def_tab: dict[plb.Path] = {}
 
     # ========== 检测命令 ==========
@@ -57,35 +65,23 @@ class EasyConsole(cmd.Cmd):
         self.fp_const.close()
         return True
 
-    # ========== 主要功能 ==========
+    # ========== 路径操作命令 ==========
 
     def do_cd(self, arg: str):
         'Open and move to a directory'
         _args = parse(arg)
         if not _args:
             _tempptr = self.path_ptr
-        elif isinstance(_args[0], str):
-            if not _args[0] in self.const_def_tab:
-                if not _args[0] in self.path_def_tab:
-                    print('Wrong index')
-                    return False
-                else:
-                    _tempptr = self.path_def_tab[_args[0]]
-            else:
-                _tempptr = self.const_def_tab[_args[0]]
         else:
-            if _args[0] < -1 or _args[0] >= self.path_num:
-                print('Wrong index')
-                return False
-            if _args[0] == -1:
-                _tempptr = self.path_ptr.parent
-            else:
-                _tempptr = self.son_paths[_args[0]]
+            _tempptr = self._pathptr_FromArg(_args[0])
+        if not _tempptr:
+            return False
+        _tempptr: plb.Path
         if not _tempptr.is_dir():
             print('Not a directory')
             return False
-        self.path_ptr=_tempptr
-        self.son_paths: list[plb.Path] = [x for x in self.path_ptr.iterdir()]
+        self.path_ptr = _tempptr
+        self.son_paths = [x for x in self.path_ptr.iterdir()]
         self.path_num = len(self.son_paths)
 
     def do_ls(self, arg: str):
@@ -93,23 +89,11 @@ class EasyConsole(cmd.Cmd):
         _args = parse(arg)
         if not _args:
             _tempptr = self.path_ptr
-        elif isinstance(_args[0], str):
-            if not _args[0] in self.const_def_tab:
-                if not _args[0] in self.path_def_tab:
-                    print('Wrong index')
-                    return False
-                else:
-                    _tempptr = self.path_def_tab[_args[0]]
-            else:
-                _tempptr = self.const_def_tab[_args[0]]
         else:
-            if _args[0] < -1 or _args[0] >= self.path_num:
-                print('Wrong index')
-                return False
-            if _args[0] == -1:
-                _tempptr = self.path_ptr.parent
-            else:
-                _tempptr = self.son_paths[_args[0]]
+            _tempptr = self._pathptr_FromArg(_args[0])
+        if not _tempptr:
+            return False
+        _tempptr: plb.Path
         if not _tempptr.is_dir():
             print('Not a directory')
             return False
@@ -118,12 +102,153 @@ class EasyConsole(cmd.Cmd):
         for i, x in enumerate(_tempptr.iterdir()):
             print(i, x)
 
+    # ========== 全局量定义命令 ==========
+
+    def do_const(self, arg: str):
+        'Define a constant path alias'
+        _args = parse(arg)
+        if len(_args) != 2 or not isinstance(_args[0], str):
+            print('Wrong sytax')
+            return False
+        _tempptr = self._pathptr_FromArg(_args[1])
+        if not _tempptr:
+            return False
+        _tempptr: plb.Path
+        self.const_def_tab[_args[0]] = _tempptr.absolute()
+        self.fp_const = open(r'ConstPaths.dat', 'wb')
+        pkl.dump(self.const_def_tab, self.fp_const)
+        self.fp_const.close()
+
+    def do_let(self, arg: str):
+        'Define a temporary path alias'
+        _args = parse(arg)
+        if len(_args) != 2 or not isinstance(_args[0], str):
+            print('Wrong syntax')
+            return False
+        _tempptr = self._pathptr_FromArg(_args[1])
+        if not _tempptr:
+            return False
+        _tempptr: plb.Path
+        self.path_def_tab[_args[0]] = _tempptr.absolute()
+
+    def do_del(self, arg: str):
+        'Delete an alias'
+        _args = parse(arg)
+        _length = len(_args)
+        if _length != 1 or not isinstance(_args[0], str):
+            if _length != 2:
+                print('Wrong syntax')
+                return False
+            elif _args[1] != 'const' and _args[1] != 'let':
+                print('Wrong syntax')
+                return False
+        if _length == 1:
+            _set = self._pathptr_FromAlias(_args[0])
+        else:
+            _set = _args[1]
+        if _set == 'const':
+            del self.const_def_tab[_args[0]]
+        else:
+            del self.path_def_tab[_args[0]]
+
+    # ========== 指令执行命令 ==========
+
+    def do_run(self, arg: str):
+        'Run a pre-processed command line'
+        _args = parse(arg)
+        for i in range(len(_args)):
+            if (_subs := self._pathptr_FromArg_Ex(_args[i])):
+                _subs: plb.Path
+                _args[i] = str(_subs.absolute())
+            elif isinstance(_args[i], int):
+                _args[i] = str(_args[i])
+        _args: list[str]
+        os.system(' '.join(_args))
+
+    def do_cmd(self, arg: str):
+        'Run a raw command line'
+        os.system(arg)
+
     # ========== 命令预处理 ==========
 
     def precmd(self, line: str) -> str:
         return line.lower()
-    
 
+    def _pathptr_FromArg(self, _index: str | int) -> plb.Path | bool:
+        if isinstance(_index, str):
+            if not _index in self.path_def_tab:
+                if not _index in self.const_def_tab:
+                    print('Wrong alias')
+                    return False
+                else:
+                    return self.const_def_tab[_index]
+            else:
+                return self.path_def_tab[_index]
+        else:
+            if _index < -1 or _index >= self.path_num:
+                print('Wrong index')
+                return False
+            if _index == -1:
+                return self.path_ptr.parent
+            else:
+                return self.son_paths[_index]
+
+    def _pathptr_FromArg_Ex(self, _index: str | int) -> plb.Path | bool:
+        if isinstance(_index, str):
+            if not _index in self.path_def_tab:
+                if not _index in self.const_def_tab:
+                    return False
+                else:
+                    return self.const_def_tab[_index]
+            else:
+                return self.path_def_tab[_index]
+        else:
+            if _index < -1 or _index >= self.path_num:
+                return False
+            if _index == -1:
+                return self.path_ptr.parent
+            else:
+                return self.son_paths[_index]
+
+    def _pathptr_FromAlias(self, _alias: str) -> str | bool:
+        if not isinstance(_alias, str):
+            return False
+        if not _alias in self.path_def_tab:
+            if not _alias in self.const_def_tab:
+                print('Wrong alias')
+                return False
+            else:
+                return 'const'
+        else:
+            return 'let'
+
+    def _pathptr_FromAlias_Ex(self, _alias: str) -> str | bool:
+        if not isinstance(_alias, str):
+            return False
+        if not _alias in self.path_def_tab:
+            if not _alias in self.const_def_tab:
+                return False
+            else:
+                return 'const'
+        else:
+            return 'let'
+
+API_TAB={
+    'help':EasyConsole.do_help,
+    'hello':EasyConsole.do_hello,
+    'exit':EasyConsole.do_exit,
+    'cd':EasyConsole.do_cd,
+    'ls':EasyConsole.do_ls,
+    'const':EasyConsole.do_const,
+    'let':EasyConsole.do_let,
+    'del':EasyConsole.do_del,
+    'run':EasyConsole.do_run,
+    'cmd':EasyConsole.do_cmd
+}
+
+def do(console:EasyConsole, topic:str, arg:str=''):
+    'EasyConsole API'
+    API_TAB[topic](console, arg)
 
 if __name__ == '__main__':
     EasyConsole().cmdloop()
--- README.md
+++ README.md
@@ -6,13 +6,23 @@ Here, you can use number or your customized symbols to represent a long and ugly
 
 These is the command we support:
 
-- help
-- hello
-- exit
-- cd
-- ls
-- const(future)
-- let(future)
-- run(future)
-
-You can install by run `setup.py`(future).
+- `help [<Topic :: str>]`
+  - Helper.
+- `hello [<Name :: str>]`
+  - To say hello. You can attach a name to it.
+- `exit [<Name :: str>]`
+  - To exit console. You can attach a name to it.
+- `cd [<Path :: int | const | val>]`
+  - To open and move to a directory. If not given any argument, it will open the current directory, like `cd .` in cmd.
+- `ls [<Path :: int | const | val>]`
+  - To list out the paths under a directory. If not given argument, it will list out the current directory.
+- `const <ConstAlias :: str> <Path :: int | const | val>`
+  - To give a path with a constant alias. Constant alias still valid the next time you use this console.
+- `let <ValAlias :: str> <Path :: int | const | val>`
+  - To give a path with a temporary alias. It will not be kept after you exit the console.
+- `del <Alias :: str> [<Set = const | let>]`
+  - To delete an alias. If not given the set, we will find it first in the set of temporary aliases then the set of constant aliases.
+- `run <SymCmd :: line(str, int, const, val)>`
+  - To run a command line. All the numbers and aliases valid will be replaced.
+- `cmd <RawCmd :: line(str)>`
+  - To run a command line in its raw state.
